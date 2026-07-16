543ef3a872de2195bac6e6fcc2fa652b0f173a86
Joseph Castro <josephc070107@gmail.com>
2026-07-16 17:40:01 -0400

    fix: export avaliable capillary adapters

--- src/primer_core/adapters/capillary/__init__.py
+++ src/primer_core/adapters/capillary/__init__.py
@@ -2,18 +2,17 @@
 
 Owners:
     PgVectorKnowledgeBase - KG-W3 (Rianna)
-    FileMemoryStore       - LD-W3 (Malakhi)
     WorkflowCliRunner     - DS-W3 (Joseph)
+
+FileMemoryStore will be exported here after LD-W3 lands.
 """
 
 from __future__ import annotations
 
-from primer_core.adapters.capillary.file_memory_store import FileMemoryStore
 from primer_core.adapters.capillary.kb_pgvector import PgVectorKnowledgeBase
 from primer_core.adapters.capillary.workflow_cli_runner import WorkflowCliRunner
 
 __all__ = [
-    "FileMemoryStore",
     "PgVectorKnowledgeBase",
     "WorkflowCliRunner",
 ]
